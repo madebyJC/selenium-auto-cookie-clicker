fb20284e029046e7f267587f1da19b1f698df31c
Jc Paredes <paredesjancarlo99@gmail.com>
2023-05-28 17:33:04 +0800

    main.py
    updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -7,11 +8,24 @@ option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
+start_time = time.time()
+running = True
+
+
+def all_store():
+    buy_store = driver.find_elements(By.CSS_SELECTOR, "#store div")
+    for n in range(len(buy_store), -1, -1):
+        try:
+            buy_store[n].click()
+        except:
+            continue
+
+
 cookie = driver.find_element(By.ID, "cookie")
 
-driver.fullscreen_window()
+while True:
+    for i in range(0, 500):
+        cookie.click()
 
-for n in range(10**10):
-    cookie.click()
-
-driver.quit()
+    for i in range(0, 5):
+        all_store()
